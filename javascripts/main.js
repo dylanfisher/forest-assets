1ce86cc31b2db2d29b39ed5b3e9cded37eb7afec
dylanfisher <hi@dylanfisher.com>
2022-02-05 11:42:47 -0500

    Update main.js to cache document and window, add breakpointChange

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,30 +4,35 @@
 
 window.App = window.App || {};
 
+App.$window = $(window);
+App.$document = $(document);
+
 App.pageLoad = [];
 App.pageResize = [];
 App.pageScroll = [];
-// App.pageThrottledScroll = [];
-// App.pageDebouncedResize = [];
+App.pageThrottledScroll = [];
+App.pageDebouncedResize = [];
+App.breakpointChange = [];
 App.teardown = [];
 App.runFunctions = function(array) {
   for (var i = array.length - 1; i >= 0; i--) {
     array[i]();
   }
 };
-App.isHomePage = function() {
-  return App.$body.hasClass('controller--home_pages');
-};
+// App.isHomePage = function() {
+//   return App.$body.hasClass('controller--home_pages');
+// };
+App.currentBreakpoint = undefined;
 
 //////////////////////////////////////////////////////////////
 // On page load
 //////////////////////////////////////////////////////////////
 
 $(function() {
-  App.scrollTop = $(window).scrollTop();
+  App.scrollTop = App.$window.scrollTop();
 
-  App.windowWidth  = $(window).width();
-  App.windowHeight = $(window).height();
+  App.windowWidth  = App.$window.width();
+  App.windowHeight = App.$window.height();
 
   App.$html = $('html');
   App.$body = $('body');
@@ -35,41 +40,61 @@ $(function() {
 
   App.$html.removeClass('no-js');
 
+  App.currentBreakpoint = App.breakpoint();
+
   App.runFunctions(App.pageLoad);
   App.runFunctions(App.pageResize);
-  // App.runFunctions(App.pageDebouncedResize);
+  App.runFunctions(App.pageDebouncedResize);
   App.runFunctions(App.pageScroll);
-  // App.runFunctions(App.pageThrottledScroll);
+  App.runFunctions(App.pageThrottledScroll);
+
+  // In some situations you may wish to add or remove functionality after a brief delay
+  // on initial page load to avoid situations where CSS transitions flash into an opacity: 0 state.
+  window.setTimeout(function() {
+    App.$html.removeClass('js-preload');
+    App.$document.trigger('app:delayed-page-load');
+  }, 200);
 });
 
 //////////////////////////////////////////////////////////////
 // On scroll
 //////////////////////////////////////////////////////////////
 
-$(window).on('scroll', function() {
-  App.scrollTop = $(window).scrollTop();
+App.$window.on('scroll', function() {
+  App.scrollTop = App.$window.scrollTop();
 
   App.runFunctions(App.pageScroll);
 });
 
-// $(window).on('scroll', $.throttle(200, function() {
-//   App.runFunctions(App.pageThrottledScroll);
-// }));
+App.$window.on('scroll', $.throttle(200, function() {
+  App.runFunctions(App.pageThrottledScroll);
+}));
 
 //////////////////////////////////////////////////////////////
 // On resize
 //////////////////////////////////////////////////////////////
 
-$(window).on('resize', function() {
-  App.windowWidth  = $(window).width();
-  App.windowHeight = $(window).height();
+App.$window.on('resize', function() {
+  App.windowWidth  = App.$window.width();
+  App.windowHeight = App.$window.height();
+
+  if ( App.currentBreakpoint != App.breakpoint() ) App.$document.trigger('app:breakpoint-change');
+  App.currentBreakpoint = App.breakpoint();
 
   App.runFunctions(App.pageResize);
 });
 
-// $(window).on('resize', $.debounce(500, function() {
-//   App.runFunctions(App.pageDebouncedResize);
-// }));
+App.$window.on('resize', $.debounce(500, function() {
+  App.runFunctions(App.pageDebouncedResize);
+}));
+
+//////////////////////////////////////////////////////////////
+// On breakpoint change
+//////////////////////////////////////////////////////////////
+
+App.$document.on('app:breakpoint-change', function() {
+  App.runFunctions(App.breakpointChange);
+});
 
 App.breakpoint = function(checkIfSize) {
   // Make sure these match the breakpoint variables set in variables.scss
